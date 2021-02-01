ea9a168663aa26800ad212abfb15cd12d27cdcbc
Alvaro Acuña <sebastian20.a.m@gmail.com>
2021-02-01 11:34:47 -0300

    Datos de EJS Cat, avance sin CSS

--- server.js
+++ server.js
@@ -10,6 +10,72 @@ app.use(express.static(__dirname + "/public"));
 app.set('views', __dirname + '/vistas');
 app.set('view engine', 'ejs');
 
+const gatos = [
+    {
+        idgato: "gato1",
+        nombre: "Anvorgueza",
+        img: "gato1.jpg",
+        comidafavorita: "Anvorgueza",
+        edad: "3 años",
+        sleepingposts: ["Arriba del sillón", "En la alfombra"]
+    },
+    {
+        idgato: "gato2",
+        nombre: "Yuumi",
+        img: "gato2.png",
+        comidafavorita: "Leeetchuga",
+        edad: "2 meses",
+        sleepingposts: ["En la cama del dueño","En la fiesta de los ratones"]
+    },
+    {
+        idgato: "gato3",
+        nombre: "Cristal",
+        img: "gato3.jpg",
+        comidafavorita: "Whiskas",
+        edad: "4 meses",
+        sleepingposts: ["Debajo del auto", "En una caja"]
+    }
+]
+
+app.get("/cats", (req,res)=>{
+    res.render("cats", { gatos : gatos });
+});
+
+app.get("/detalle", (req,res)=>{
+    res.render("detalle", { gatos : gatos});
+});
+
+app.get("/detalle/:id", (req,res)=> {
+    const idgatos = req.params.id;
+    res.send(
+        `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Gato: ${gatos[idgatos].idgato} </title>
+            <link rel="stylesheet" href= "cats.css">
+        </head>
+        <body>
+        <h1> ¡Bienvenido a mi página de Meme Gatos! </h1>
+        <img src= "images/${gatos[idgatos].img}" alt="${gatos[idgatos].idgato}">
+        <h3>Nombre: ${gatos[idgatos].nombre}</h3>
+        <h3>Comida Favorita: ${gatos[idgatos].comidafavorita}</h3>
+        <h3>Edad: ${gatos[idgatos].edad}</h3>
+        <ul>
+            <li>${gatos[idgatos].sleepingposts[0]}</li>
+            <li>${gatos[idgatos].sleepingposts[1]}</li>
+        </ul
+    
+        </body>
+        </html>`
+    );
+
+});
+
+
+
+
 app.get("/index" , (req,res)=> {
 
     res.render("vistaservidor");
@@ -49,6 +115,9 @@ app.get("/cars" , (req,res)=> {
     res.render("cars", paginaCars );
 
 });
+
+
+
 
 
 
