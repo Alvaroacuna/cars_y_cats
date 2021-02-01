8d9b21d536f86cf669f96b8c0122a6da651d2eee
Alvaro Acuña <sebastian20.a.m@gmail.com>
2021-01-31 23:44:51 -0300

    Segundo commit, EJS Cars y Cats

--- server.js
+++ server.js
@@ -7,6 +7,49 @@ const port = 8000;
 
 app.use(express.static(__dirname + "/public"));
 
+app.set('views', __dirname + '/vistas');
+app.set('view engine', 'ejs');
+
+app.get("/index" , (req,res)=> {
+
+    res.render("vistaservidor");
+
+});
+
+app.get("/cats" , (req,res)=> {
+
+    let paginaCats = {
+        
+        titulo: "Bienvenido a la página de Cats",    
+        contenido: "Esto es la página principal de Cats, podrás encontrar variedad de memes de gatos",
+        gatouno: "images/gato1.jpg",
+        gatodos: "images/gato2.png",
+        gatotres: "images/gato3.jpg"
+
+    }
+
+    
+    res.render("cats", paginaCats );
+
+});
+
+app.get("/cars" , (req,res)=> {
+
+    let paginaCars = {
+        
+        titulo: "Bienvenido a la página de Cars",    
+        contenido: "Esto es la página principal de Cars, podrás encontrar variedad de vehículos deportivos",
+        autouno: "images/ferrari.jpg",
+        autodos: "images/lamborghini.jpg",
+        autotres: "images/mustang.jpg"
+
+    }
+
+    
+    res.render("cars", paginaCars );
+
+});
+
 
 
 
